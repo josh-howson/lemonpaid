8c56b5548ac27d75f34cc186576dcd0826c129f8
josh-howson <52839079+josh-howson@users.noreply.github.com>
2024-11-29 00:02:54 -0800

    open app to /onboard

--- pages/onboard/index.vue
+++ pages/onboard/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <OnboardingStep>
+  <OnboardStep>
     <template #hero>
-      <img class="graphic-1 slide-in-from-left" src="/images/onboarding/onboard-1-1.png" alt="">
+      <img class="graphic-1 slide-in-from-left" src="/images/onboard/onboard-1-1.png" alt="">
 
-      <img class="graphic-2 slide-in-from-right" :style="{ '--delay': '1.5s' }" src="/images/onboarding/onboard-1-2.png" alt="">
+      <img class="graphic-2 slide-in-from-right" :style="{ '--delay': '1.5s' }" src="/images/onboard/onboard-1-2.png" alt="">
     </template>
     <template #message>
       <h1 class="headline-1">Bring back the joy of earning</h1>
@@ -12,7 +12,7 @@
 
       <button class="button-primary">Continue</button>
     </template>
-  </OnboardingStep>
+  </OnboardStep>
 </template>
 
 <style scoped>
